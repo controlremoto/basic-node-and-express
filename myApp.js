564845719c4a4d4f4320477b25854dca13b2b613
Miguel <miguel3lp@hotmail.com>
2022-06-13 17:46:17 -0400

    feat:Implement a Root-Level Request Logger Middleware
    #7 Implement a Root-Level Request Logger Middleware, will log method, path and the user ip address

--- myApp.js
+++ myApp.js
@@ -9,15 +9,21 @@ const abs_html_path = __dirname + '/views/index.html'
 // Define static aboslute path
 const abs_static_path = __dirname + '/public';
 
-app.get('/', (req,res) => res.sendFile(abs_html_path));
-app.use('/public',express.static(abs_static_path));
-app.use('/json', (req,res) => {
-    if(process.env.MESSAGE_STYLE === 'uppercase') {
-        res.json({"message": "HELLO JSON"});
+// The root-top-middleware
+app.use((req, res, next) => {
+    console.log(req.method + " " + req.path + " - " + req.ip);
+    next();
+})
+
+app.get('/', (req, res) => res.sendFile(abs_html_path));
+app.use('/public', express.static(abs_static_path));
+app.use('/json', (req, res) => {
+    if (process.env.MESSAGE_STYLE === 'uppercase') {
+        res.json({ "message": "HELLO JSON" });
     } else {
-        res.json({"message": "Hello json"});
+        res.json({ "message": "Hello json" });
     }
-} )
+})
 
 
 
