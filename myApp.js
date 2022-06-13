5f1d4ff4977f5fbdadb9dcade218ae1e96653a34
Miguel <miguel3lp@hotmail.com>
2022-06-13 17:15:45 -0400

    feat:Use the .env File
    #6 Use the .env File will return a different object depending on environment variable

--- myApp.js
+++ myApp.js
@@ -1,5 +1,7 @@
 let express = require('express');
 let app = express();
+require('dotenv').config();
+
 
 // Define html absolute path
 const abs_html_path = __dirname + '/views/index.html'
@@ -10,9 +12,11 @@ const abs_static_path = __dirname + '/public';
 app.get('/', (req,res) => res.sendFile(abs_html_path));
 app.use('/public',express.static(abs_static_path));
 app.use('/json', (req,res) => {
-    res.json({
-        "message": "Hello json"
-    })
+    if(process.env.MESSAGE_STYLE === 'uppercase') {
+        res.json({"message": "HELLO JSON"});
+    } else {
+        res.json({"message": "Hello json"});
+    }
 } )
 
 
